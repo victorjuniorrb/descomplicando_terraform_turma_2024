5093440fb1911220056d72c7d83732697b3bdf75
Victor Júnior R. B. <victorjunior@gmail.com>
2024-07-06 00:07:11 -0300

    update env

--- dia_05/aula_01_modules_source/iac/terraform/dev/main.tf
+++ dia_05/aula_01_modules_source/iac/terraform/dev/main.tf
@@ -1,5 +1,5 @@
 module "projetoa" {
   source      = "git@github.com:victorjuniorrb/modulo_instancias.git//modules/instancia?ref=v2.0.0"
   nome        = "projeto_alpha"
-  environment = "Dev"
+  environment = "dev"
 }
