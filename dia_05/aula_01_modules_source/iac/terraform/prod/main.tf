340c9d839353c56076edf285dae8041b00a9d1d8
Victor Júnior R. B. <victorjunior@gmail.com>
2024-07-06 00:07:37 -0300

    use version v1.0.0

--- dia_05/aula_01_modules_source/iac/terraform/prod/main.tf
+++ dia_05/aula_01_modules_source/iac/terraform/prod/main.tf
@@ -1,5 +1,5 @@
 module "projetoa" {
-  source      = "git@github.com:victorjuniorrb/modulo_instancias.git"
+  source      = "git@github.com:victorjuniorrb/modulo_instancias.git?ref=v1.0.0"
   nome        = "projetoA"
   environment = "Production"
 }
