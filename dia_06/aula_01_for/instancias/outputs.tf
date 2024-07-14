66b5e526e4993b5423ba73b90f731cd906f0e0a9
Victor Júnior R. B. <victorjunior@gmail.com>
2024-07-14 19:52:10 -0300

    change file

--- dia_06/aula_01_for/instancias/outputs.tf
+++ dia_06/aula_01_for/instancias/outputs.tf
@@ -1,5 +1,6 @@
 output "public_ip" {
   description = "IPs públicos"
+  # value       = {for instance in aws_instance.this : instance.tags["Name"] => instance.public_ip}
+  # value       = { for key, instance in aws_instance.this : key => instance.public_ip }
   value       = { for key, instance in aws_instance.this : instance.tags["Env"] => instance.public_ip... }
 }
-
