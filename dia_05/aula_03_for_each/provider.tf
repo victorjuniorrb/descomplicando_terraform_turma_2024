d8fbd660620213020865ed989486a84dc30f503a
Victor Júnior R. B. <victorjunior@gmail.com>
2024-07-14 17:34:21 -0300

    update file

--- dia_05/aula_03_for_each/provider.tf
+++ dia_05/aula_03_for_each/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "descomplicando-terraform-turma-2024"
+    bucket = "descomplicando-terraform-turma-2024-noturno"
     key    = "aula_for_each"
     region = "us-east-1"
   }
